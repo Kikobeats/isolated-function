fdc11b650409afc710bacc0cfc485f88564f6d1e
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-08-25 21:43:11 +0200

    chore: pass globals

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ const { deserializeError } = require('serialize-error')
 
 const compile = require('./compile')
 
-module.exports = (snippet, { timeout = 10000 } = {}) => {
-  const sandbox = new Sandbox()
-  const codePromise = compile(snippet)
+module.exports = (
+  snippet,
+  {
+    timeout = 10000,
+    httpEnabled = false,
+    timersEnabled = true,
+    globals = {}
+  } = {}
+) => {
+  const sandbox = new Sandbox({ httpEnabled, timersEnabled, debug: true })
+  const compiling = compile(snippet)
+  const initializing = sandbox.initialize()
 
   return async (...args) => {
-    const code = await codePromise
+    const [code] = await Promise.all([compiling, initializing])
     const { error, value } = await sandbox.execute({
       code,
       timeout,
-      globals: { arguments: args }
+      globals: { ...globals, arguments: args }
     })
     await sandbox.shutdown()
     if (error) throw require('ensure-error')(deserializeError(error))
