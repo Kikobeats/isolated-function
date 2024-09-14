5f0388779ddda0abd3f474786d64dfad483fa7b8
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-09-14 09:56:47 +0200

    refactor: expose builder

--- src/compile/index.js
+++ src/compile/index.js
@@ -1,34 +1,14 @@
 'use strict'
 
-const { execSync } = require('child_process')
-const esbuild = require('esbuild')
 const fs = require('fs/promises')
-const $ = require('tinyspawn')
 const path = require('path')
 
 const transformDependencies = require('./transform-dependencies')
 const detectDependencies = require('./detect-dependencies')
+const installDependencies = require('./install-dependencies')
 const generateTemplate = require('../template')
 const { duration } = require('../debug')
-
-const MINIFY = (() => {
-  return process.env.ISOLATED_FUNCTIONS_MINIFY !== 'false'
-    ? {}
-    : {
-        minifyWhitespace: true,
-        minifyIdentifiers: false,
-        minifySyntax: true
-      }
-})()
-
-const install = (() => {
-  try {
-    execSync('which pnpm').toString().trim()
-    return 'pnpm install'
-  } catch {
-    return 'npm install'
-  }
-})()
+const build = require('./build')
 
 const tmpdirDefault = async () => {
   const cwd = await fs.mkdtemp(path.join(require('os').tmpdir(), 'compile-'))
@@ -43,25 +23,8 @@ module.exports = async (snippet, tmpdir = tmpdirDefault) => {
 
   const content = transformDependencies(compiledTemplate)
   const tmpDir = await duration('tmpdir', tmpdir)
-
-  await duration('npm:init', () => fs.writeFile(path.join(tmpDir.cwd, 'package.json'), '{}'))
-  await duration('npm:install', () =>
-    $(`${install} ${dependencies.join(' ')}`, { cwd: tmpDir.cwd })
-  )
-
-  const result = await duration('esbuild', () =>
-    esbuild.build({
-      stdin: {
-        contents: content,
-        resolveDir: tmpDir.cwd,
-        sourcefile: 'index.js'
-      },
-      bundle: true,
-      ...MINIFY,
-      write: false,
-      platform: 'node'
-    })
-  )
+  await duration('npm:install', () => installDependencies({ dependencies, cwd: tmpDir.cwd }))
+  const result = await duration('esbuild', () => build({ content, cwd: tmpDir.cwd }))
 
   return {
     content: result.outputFiles[0].text,
