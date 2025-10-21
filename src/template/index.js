318a4bfeee31412f30c8cf95baf65b64dd8a626a
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-10-21 11:10:23 +0200

    chore: debug

--- src/template/index.js
+++ src/template/index.js
@@ -4,35 +4,52 @@ const SERIALIZE_ERROR = require('./serialize-error')
 
 module.exports = snippet => `
   ;(async () => {
-    const args = JSON.parse(process.argv[2])
+    try {
+      const args = JSON.parse(process.argv[2])
 
-    /* https://github.com/Kikobeats/null-prototype-object */
-    const logging = new (/* @__PURE__ */ (() => { let e = function(){}; return e.prototype = Object.create(null), Object.freeze(e.prototype), e })());
+      /* https://github.com/Kikobeats/null-prototype-object */
+      const logging = new (/* @__PURE__ */ (() => { let e = function(){}; return e.prototype = Object.create(null), Object.freeze(e.prototype), e })());
 
-    for (const method of ['log', 'info', 'debug', 'warn', 'error']) {
-      console[method] = function (...args) {
-        logging[method] === undefined ? logging[method] = [args] : logging[method].push(args)
+      for (const method of ['log', 'info', 'debug', 'warn', 'error']) {
+        console[method] = function (...args) {
+          logging[method] === undefined ? logging[method] = [args] : logging[method].push(args)
+        }
       }
+
+      ;await (async (send) => {
+        process.stdout.write = function () {}
+        let value
+        let isFulfilled
+
+        try {
+          value = await (${snippet.toString()})(...args)
+          isFulfilled = true
+        } catch (error) {
+          value = ${SERIALIZE_ERROR}(error)
+          isFulfilled = false
+        } finally {
+          send(JSON.stringify({
+            isFulfilled,
+            logging,
+            value,
+            profiling: { memory: process.memoryUsage().rss }
+          }))
+        }
+      })(process.stdout.write.bind(process.stdout))
+    } catch (error) {
+      process.stdout.write(JSON.stringify({
+        isFulfilled: false,
+        logging: {},
+        value: ${SERIALIZE_ERROR}(error),
+        profiling: { memory: process.memoryUsage().rss }
+      }))
     }
-
-    ;await (async (send) => {
-      process.stdout.write = function () {}
-      let value
-      let isFulfilled
-
-      try {
-        value = await (${snippet.toString()})(...args)
-        isFulfilled = true
-      } catch (error) {
-        value = ${SERIALIZE_ERROR}(error)
-        isFulfilled = false
-      } finally {
-        send(JSON.stringify({
-          isFulfilled,
-          logging,
-          value,
-          profiling: { memory: process.memoryUsage().rss }
-        }))
-      }
-    })(process.stdout.write.bind(process.stdout))
-  })()`
+  })().catch(err => {
+    process.stdout.write(JSON.stringify({
+      isFulfilled: false,
+      logging: {},
+      value: ${SERIALIZE_ERROR}(err),
+      profiling: { memory: process.memoryUsage().rss }
+    }))
+    process.exitCode = 1
+  })`
