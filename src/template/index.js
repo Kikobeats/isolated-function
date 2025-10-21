d2312546015d90fc0b0216a9fd646da7fda3122d
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-10-21 10:37:12 +0200

    fix: ensure to wait

--- src/template/index.js
+++ src/template/index.js
@@ -3,34 +3,36 @@
 const SERIALIZE_ERROR = require('./serialize-error')
 
 module.exports = snippet => `
-  const args = JSON.parse(process.argv[2])
+  ;(async () => {
+    const args = JSON.parse(process.argv[2])
 
-  /* https://github.com/Kikobeats/null-prototype-object */
-  const logging = new (/* @__PURE__ */ (() => { let e = function(){}; return e.prototype = Object.create(null), Object.freeze(e.prototype), e })());
+    /* https://github.com/Kikobeats/null-prototype-object */
+    const logging = new (/* @__PURE__ */ (() => { let e = function(){}; return e.prototype = Object.create(null), Object.freeze(e.prototype), e })());
 
-  for (const method of ['log', 'info', 'debug', 'warn', 'error']) {
-    console[method] = function (...args) {
-      logging[method] === undefined ? logging[method] = [args] : logging[method].push(args)
+    for (const method of ['log', 'info', 'debug', 'warn', 'error']) {
+      console[method] = function (...args) {
+        logging[method] === undefined ? logging[method] = [args] : logging[method].push(args)
+      }
     }
-  }
 
-  ;(async (send) => {
-    process.stdout.write = function () {}
-    let value
-    let isFulfilled
+    ;await (async (send) => {
+      process.stdout.write = function () {}
+      let value
+      let isFulfilled
 
-    try {
-      value = await (${snippet.toString()})(...args)
-      isFulfilled = true
-    } catch (error) {
-      value = ${SERIALIZE_ERROR}(error)
-      isFulfilled = false
-    } finally {
-      send(JSON.stringify({
-        isFulfilled,
-        logging,
-        value,
-        profiling: { memory: process.memoryUsage().rss }
-      }))
-    }
-  })(process.stdout.write.bind(process.stdout))`
+      try {
+        value = await (${snippet.toString()})(...args)
+        isFulfilled = true
+      } catch (error) {
+        value = ${SERIALIZE_ERROR}(error)
+        isFulfilled = false
+      } finally {
+        send(JSON.stringify({
+          isFulfilled,
+          logging,
+          value,
+          profiling: { memory: process.memoryUsage().rss }
+        }))
+      }
+    })(process.stdout.write.bind(process.stdout))
+  })()`
